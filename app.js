3f4ca2cece32da4aafb41f1b1a237866a222b28f
Agustina Valentin <valentinagustina3@gmail.com>
2022-07-25 23:28:59 -0300

    envio de formulario

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var nosotrosRouter = require ('./routes/nosotros');
 var productosRouter = require ('./routes/productos');
--- routes/contacto.js
+++ routes/contacto.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer= require ('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,5 +8,37 @@ router.get('/', function(req, res, next) {
     isContacto:true
   });
 });
+
+router.post('/', async function(req, res, next){
+ console.log(req.body)
+
+ var nombre=req.body.nombre;
+ var email=req.body.email;
+ var telefono=req.body.telefono;
+ var intereses=req.body.intereses;
+ var encuesta=req.body.encuesta;
+ var comentarios=req.body.comentarios;
+
+ var obj= {
+  to:'valentinagustina3@gmail.com',
+  subjetc:'contacto desde la pagina web',
+  html: nombre +"se contacto a traves de la web y quiere saber mas info a este correo:"+ email + ".<br> su tel es:"
+  +telefono + ".le interesa:" + intereses + ".nos conocio a traves de:" + encuesta + ".su comentario es:"+ comentarios + "."
+ }
+ var transport= nodemailer.createTransport({
+  host:process.env.SMTP_HOST,
+  port:process.env.SMTP_PORT,
+  auth:{
+    user:process.env.SMTP_USER,
+    pass:process.env.SMTP_PASS
+  }
+ })
+ var info= await transport.sendMail(obj);
+
+ res.render('contacto',{
+  message:'Mensaje enviado correctamente',
+  isContacto:true
+ })
+})
 
 module.exports = router;
