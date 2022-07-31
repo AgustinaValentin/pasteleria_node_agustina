cb81905711d0e4c45b0f3eed495764e299c143c0
Agustina Valentin <valentinagustina3@gmail.com>
2022-07-31 14:04:19 -0300

    novedades dinamico

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel= require ('../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
   res.render('novedades',{
-    isNovedades:true
+    isNovedades:true,
+    novedades
   });
 });
 
