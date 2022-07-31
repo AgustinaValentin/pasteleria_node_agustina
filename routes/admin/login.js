e9c47f8b16d9aefe77fee9829e608518e723686d
Agustina Valentin <valentinagustina3@gmail.com>
2022-07-31 13:11:44 -0300

    cerrar sesion

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -9,6 +9,14 @@ router.get('/', function(req, res, next) {
   }) 
 });
 
+router.get('/logout',function(req, res, next){
+  req.session.destroy();
+  res.render('admin/login',{
+    layout:'admin/layout'
+  })
+
+})
+
 router.post('/', async function(req, res, next){
   try{
     console.log(req.body);
